d4496edcca4caa6f0c3fc769027fde94c26a8b8b
Yazeed Kelly <yazeedkelly@hotmail.com>
2017-09-28 16:20:03 +0000

    initial commit

---
+++ js/app.js
@@ -0,0 +1,21 @@
+angular.module('TodoApp',['ngRoute', 'RouteControllers', "UserService", "angular-storage", "TodoService"]);
+
+angular.module('TodoApp').config(function($locationProvider, $routeProvider) { $locationProvider.html5Mode(true);
+    
+    $routeProvider.when('/', {
+        templateUrl: 'templates/home.html',
+        controller: 'HomeController'
+    })
+    .when('/todo', {
+        templateUrl: 'templates/todo.html',
+        controller: 'TodoController'
+    })
+    .when("/todo/edit/:id", {
+        templateUrl: "templates/edit-todo.html",
+        controller: "Edit.TodoController"
+    })
+    .when('/accounts/register', {
+        templateUrl: 'templates/register.html',
+        controller: 'RegisterController'
+    });
+});
---
+++ readme.md
@@ -0,0 +1,14 @@
+# **Markdown**
+### Coding Language of the FUTURE
+
+1. Three Rings for the Elven-kings under the sky
+2. Seven for the Dwarf-lords in their halls of stone
+3. Nine for Mortal Men doomed to die
+4. One for the Dark Lord on his dark throne
+5. In the Land of Mordor where the Shadows lie
+6. **One Ring to rule them all, One Ring to find them, One Ring to bring them all, and in the darkness bind them,
+In the Land of Mordor where the Shadows lie.**
+***
+[**Soul Link to the Dark L0rDz homepage yo**](http://lotr.wikia.com/wiki/Main_Page)
+
+![DARK TEXT](http://1nova.com/wallpapers/wp-content/uploads/sites/34/2015/02/One-Ring-to-Rule-them-All.jpg "Logo Title Text 1")

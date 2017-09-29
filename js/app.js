ae59d75e72fbe87c689e91fd0b2c9c839d595a8c
Yazeed Kelly <yazeedkelly@hotmail.com>
2017-09-29 09:36:51 +0000

    FINAL

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-angular.module('TodoApp',['ngRoute', 'RouteControllers', "UserService", "angular-storage", "TodoService, Todo-Directive"]);
+angular.module('TodoApp',['ngRoute', 'RouteControllers', "UserService", "angular-storage", "TodoService", "Todo-Directive"]);
 
 angular.module('TodoApp').config(function($locationProvider, $routeProvider) { $locationProvider.html5Mode(true);
     
